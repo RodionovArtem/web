c48f5db53f860866a7efd2530044e43edea9a645
RodionovArtem <125537352+RodionovArtem@users.noreply.github.com>
2023-05-04 10:32:21 +0300

    Task 9 (#8)
    * javascript
    
    * node server task
    
    * Забыл создать новую ветку, задание 5 в последнем commit
    
    * Задание 6
    
    * Задание 6
    
    * Задание 6
    
    * Задание 6
    
    * Задание 6
    
    * Задание 6
    
    * Задание 8
    
    * Задание 9
    
    ---------
    
    Co-authored-by: Rodionov_Artem <rodionovas@mstu.edu.ru>
    Co-authored-by: RodionovAS07 <artem07052015@gmail.com>

---
+++ armageddon-app/src/components/card/AsteroidContent/AsteroidContent.jsx
@@ -0,0 +1,24 @@
+import styles from "./AsteroidContent.module.css"
+export const AsteroidContent = (props) =>{
+
+    const {name, date, distance, size,distanceMode} = props;
+
+    return (
+        <div>
+            <div className={styles.contentName}>
+                {name}
+            </div>
+            <div className={styles.contentWrapper}>
+                <div className={styles.contentDate}>
+                    {`Дата: ${date}`}
+                </div>
+                <div className={styles.contentDistance}>
+                    {distanceMode ? `Расстояние: ${distance.kilometers} км` : `Расстояние: ${distance.lunar} Лун`}
+                </div>
+                <div className={styles.contentSize}>
+                    {`Размер: ${size} м`}
+                </div>
+            </div>
+        </div>
+    )
+}
