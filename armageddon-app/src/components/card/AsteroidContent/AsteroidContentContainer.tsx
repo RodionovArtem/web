df494109953be4897a3cad1bc0b4084013152fd2
RodionovArtem <artem07052016@mail.ru>
2023-06-08 09:04:44 +0300

    Updates

--- armageddon-app/src/components/card/AsteroidContent/AsteroidContentContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { AsteroidContent } from './AsteroidContent';
-import { useContext } from 'react';
-import { AsteroidsContext } from '../../asteroids-context/AsteroidsContext';
-
-export const AsteroidContentContainer = (props) => {
-
-  const {distanceMode} = useContext(AsteroidsContext)
-  return <AsteroidContent {...props} distanceMode={distanceMode}/>
-}
